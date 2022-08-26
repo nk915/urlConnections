70e5033809632ff8e6c15455213da89aab03af37
devnk915 <devnk915@gmail.com>
2022-08-26 12:22:23 +0900

    init program

---
+++ README.md
@@ -0,0 +1,10 @@
+# 실행 전 의존 패키지 설치 리스트
+## pip install selenium
+## pip install chromedriver-autoinstaller 
+## pip install bs4
+
+
+
+# 실행 방법
+1. url_list.txt : url 정보 등록
+2. python url_conn.py 실행
---
+++ UrlConnections.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
+
+def url_conn(_url_list):
+	options = Options()
+	options.add_argument('headless'); # headless는 화면이나 페이지 이동을 표시하지 않고 동작하는 모드
+
+	# webdirver 설정(Chrome, Firefox 등)
+	chromedriver_autoinstaller.install()
+	#driver = webdriver.Chrome(options=options) # 브라우저 창 안보이기
+	driver = webdriver.Chrome() # 브라우저 창 보이기
+
+	# 크롬 브라우저 내부 대기 (암묵적 대기)
+	driver.implicitly_wait(5)
+
+	# 브라우저 사이즈
+	driver.set_window_size(1920,1280)
+
+	# 페이지 이동(열고 싶은 URL)
+  
+
+	for _url in _url_list:
+		#print("conn: " + _url)
+		driver.get(_url)
+
+	time.sleep(2)
+	# 브라우저 종료
+	driver.close()       
+
+def read_url_list(_file_path):
+        f = open(_file_path, 'r')
+        url_list = list()
+        
+        lines = f.readlines()
+        
+        for line in lines:
+                line = line.strip()
+                if 'http' in line:
+                        url_list.append(line)
+
+        f.close()
+        return url_list
+
+url_conn(read_url_list("UrlList.txt"))
+
